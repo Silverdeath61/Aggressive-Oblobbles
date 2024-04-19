a39cd42b77f42c059362e2344a20fc0e29a3d444
Silverdeath61 <45100777+Silverdeath61@users.noreply.github.com>
2024-04-19 02:51:30 -0600

    The mod's actual code

---
+++ ModClass.cs
@@ -0,0 +1,47 @@
+﻿using Modding;
+using System.Collections;
+using UnityEngine;
+using HutongGames.PlayMaker;
+
+namespace AgressiveOblobbles
+{
+    public class AgressiveOblobbles : Mod 
+    {
+        public override void Initialize()
+        {
+            On.GameManager.OnNextLevelReady += OblobbleRage; //Dont forget to call orig(self) :b
+            Log("Initialized");
+        }
+        public void OblobbleRage( On.GameManager.orig_OnNextLevelReady orig, GameManager self ) 
+        {
+            if(UnityEngine.SceneManagement.SceneManager.GetActiveScene().name == "GG_Oblobbles")
+            {
+                orig(self);
+                Patience patience = new();
+                patience.Start();
+            }
+            else
+            {
+                orig(self);
+            }
+        }
+        public class Patience : MonoBehaviour
+        {
+            public void Start()
+            {
+                GameManager.instance.StartCoroutine(WaitSome());
+            }
+            public IEnumerator WaitSome()
+            {   
+                yield return new WaitForSeconds(2f);
+                GameObject mainBee = GameObject.Find("Mega Fat Bee");
+                GameObject mainBee1 = GameObject.Find("Mega Fat Bee (1)");
+
+                FsmEvent fetchedEvent = FsmEvent.FindEvent("ZERO HP");
+                
+                FSMUtility.SendEventToGameObject(mainBee, fetchedEvent);
+                FSMUtility.SendEventToGameObject(mainBee1, fetchedEvent);
+            }
+        }
+    }
+}
